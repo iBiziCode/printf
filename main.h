5ac5cbe2eed2e9d5baf9c0555cf7ed408daa1f5c
Bizimana Ibrahim <bizimanaibrahim0@gmail.com>
2023-04-24 15:23:17 +0200

    fix header

--- main.h
+++ main.h
@@ -2,7 +2,10 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
+int _putchar(char c);
 int print_char(va_list args);
 int print_string(va_list args);
 int _printf(const char *format, ...);
