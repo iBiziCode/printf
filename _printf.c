ce3e0b761cf3405eb3e3a760d825685895b5414f
Bizimana Ibrahim <bizimanaibrahim0@gmail.com>
2023-04-24 13:53:24 +0200

    handle c,s and % conversion specifiers

---
+++ _printf.c
@@ -0,0 +1,83 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+
+/**
+  * print_char - prints a single char
+  * @args: va_list containing the character to print
+  *
+  * Return: 1
+  */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+	return (1);
+}
+
+
+/**
+ * print_string - prints a string
+ * @args: va_list containing the string to print
+ *
+ * Return: void
+ */
+int print_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+	int len = 0;
+
+	while (*s)
+	{
+		write(1, s, 1);
+		s++;
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * _printf - prints formatted output to stdout
+ * @format: format string
+ *
+ * Return: number of characters printed (excluding null byte)
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	if ((format[0] == '%' && format[1] == '\0') || !format)
+		return (-1);
+
+	for (; *format; format++)
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			if (*format == 'c')
+				count += print_char(args);
+			else if (*format == 's')
+				count += print_string(args);
+			else if (*format == '%')
+				count += write(1, "%", 1);
+			else
+				count += write(1, "%r", 2);
+		}
+		else
+		{
+			count += write(1, format, 1);
+		}
+	}
+
+	va_end(args);
+
+	return (count);
+}
